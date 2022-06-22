5f518b6b9768eaa1a0f12df0c36f757f80a30dfb
FacundoAraujo25 <facundoaraujo96@gmail.com>
2022-06-22 15:54:00 -0300

    archivos base

---
+++ src/main/java/com/NoAutenticados/RoyalOak/RoyalOakApplication.java
@@ -0,0 +1,13 @@
+package com.NoAutenticados.RoyalOak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoyalOakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoyalOakApplication.class, args);
+	}
+
+}
