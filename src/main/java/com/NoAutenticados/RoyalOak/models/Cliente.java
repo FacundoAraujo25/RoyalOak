09e68c0d36d3e30f4d8660e945ca204c08c6a114
FacundoAraujo25 <facundoaraujo96@gmail.com>
2022-06-22 20:22:56 -0300

    back cliente y producto

--- src/main/java/com/NoAutenticados/RoyalOak/models/Cliente.java
+++ src/main/java/com/NoAutenticados/RoyalOak/models/Cliente.java
@@ -9,4 +9,57 @@ import javax.persistence.Id;
 
 @Entity
 public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
+    private long id;
+
+    private String nombre,apellido,email,telefono;
+
+
+    public Cliente () {}
+    public Cliente(String nombre, String apellido, String email, String telefono) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.email = email;
+        this.telefono = telefono;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
 }
