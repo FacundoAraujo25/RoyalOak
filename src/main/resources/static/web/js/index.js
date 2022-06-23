d150c2dd939d1051ccbb46065dd579abd3e0d115
nachoferrero1 <nachoferreronf699@gmail.com>
2022-06-23 18:24:30 -0300

    commit

---
+++ src/main/resources/static/web/js/index.js
@@ -0,0 +1,27 @@
+const app = Vue.createApp({
+
+    //CREAR Y USAR VARIABLES
+    data() {
+        return {
+            
+        }
+    },
+
+    created(){
+
+    },
+
+
+    methods: {
+        
+
+    },
+
+    computed: {
+
+    },
+    
+
+}).mount('#app')
+
+
