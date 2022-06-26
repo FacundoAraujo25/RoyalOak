55e27305a04b0aeda13a03f0f341a4f1d2b2bc89
nachoferrero1 <nachoferreronf699@gmail.com>
2022-06-26 19:19:19 -0300

    agregue footer,totalmente  responsive, mejoras en general de diseño

--- src/main/resources/static/web/js/index.js
+++ src/main/resources/static/web/js/index.js
@@ -14,6 +14,15 @@ const app = Vue.createApp({
 
     methods: {
         
+        mostrarIngredientes(){
+            Swal.fire({
+                title:'Nombre Hamburguesa',
+                text: 'Una hamburguesa es un sándwich hecho a base de carne molida o de origen vegetal, aglutinada en forma de filete cocinado a la parrilla o a la plancha, aunque también puede freírse u hornearse. Fuera del ámbito de habla hispana, es más común encontrar la denominación estadounidense burger, acortamiento de hamburger.',
+                confirmButtonText: 'Entendido',
+                footer: '<a href="">Agregar al carro</a>'
+              })
+        }
+        
 
     },
 
