24f4d970b0d16f1665d076ae58822964b8af8314
nachoferrero1 <nachoferreronf699@gmail.com>
2022-06-26 22:18:10 -0300

    prueba

--- src/main/resources/static/web/js/index.js
+++ src/main/resources/static/web/js/index.js
@@ -21,7 +21,40 @@ const app = Vue.createApp({
                 confirmButtonText: 'Entendido',
                 footer: '<a href="">Agregar al carro</a>'
               })
-        }
+        },
+
+        mostrarCategorias(){
+            Swal.fire({
+                html:
+                  '<div id="pizza">' +
+                  '<img src="https://cdn-icons-png.flaticon.com/512/1404/1404945.png" alt="1">' +
+                  '<img src="https://cdn-icons-png.flaticon.com/512/198/198416.png" alt="2">' +
+        
+                        '<img src="https://cdn-icons-png.flaticon.com/512/3076/3076134.png" alt="3">'+
+        
+                        '<img src="https://cdn-icons-png.flaticon.com/512/1691/1691169.png" alt="4">'+
+                                
+                        '<img src="https://cdn.pixabay.com/photo/2014/12/21/23/34/swiss-cheese-575541_1280.png" alt="5">'+
+        
+                        '<img src="https://cdn3.iconfinder.com/data/icons/food-drink-56/100/shushi-512.png" alt="6">'+
+        
+                        '<img src="https://cdn-icons-png.flaticon.com/512/941/941758.png" alt="7">'+
+                                
+                        '<img src="https://images.vexels.com/media/users/3/185216/isolated/preview/577e52c9f916cf36bed97b3df404c214-icono-de-papas-fritas.png" alt="8">'+
+                                
+                        '<img src="https://cdn-icons-png.flaticon.com/512/184/184559.png" alt="9">'+
+                                
+                        '<img src="https://cdn-icons-png.flaticon.com/512/4515/4515697.png" alt="10">' +                    
+        
+                        '<img src="https://i.pinimg.com/originals/67/ac/6f/67ac6ff9c411816ed8bbc9ae34368e8f.png" alt="11">' +                   
+        
+                        '<img src="https://images.vexels.com/media/users/3/143088/isolated/preview/f565debc52083dacca60da22284e4083-icono-de-pierna-de-pollo.png" alt="12">' +                    
+                '</div>'
+                
+              })
+        },
+
+        
         
 
     },
